19a2f4c7663d673982270ba8880d4a7aa4408d5e
backflipper1991 <sashaulych@gmail.com>
2020-05-26 21:11:55 +0300

    pagination on users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,15 +4,42 @@ import * as axios from 'axios';
 
 class Users extends React.Component {
     componentDidMount() {
-        console.log('mount');
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.totalUsersCount}`).then(response => {
             this.props.setUsers(response.data.items);
+
+            if (response.data.totalCount > 30) {
+                this.props.setTotalUsersCount(30);
+            } else {
+                this.props.setTotalUsersCount(response.data.totalCount);
+            }
+            
         });
     }
     componentDidUpdate() {
 
     }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.totalUsersCount}`).then(response => {
+            this.props.setUsers(response.data.items);
+
+            if (response.data.totalCount > 30) {
+                this.props.setTotalUsersCount(30);
+            } else {
+                this.props.setTotalUsersCount(response.data.totalCount);
+            }
+        });
+    }
     render () {
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        console.log(pagesCount);
+        let pages = [];
+
+        for (let i=1; i < pagesCount; i++) {
+            pages.push(i);
+        } 
+
         return <div>
             {
                 this.props.users.map(u => <div key={u.id} className={styles.userItem}>
@@ -42,6 +69,13 @@ class Users extends React.Component {
                     </span>
                 </div>)
             }
+            <div className={styles.pagination}>
+                {
+                    pages.map(p => {
+                        return <span onClick={() => { this.onPageChanged(p) }} className={this.props.currentPage === p && styles.selectedPage}>{p}</span>
+                    })
+                }
+            </div>
         </div>;
     }
 }
