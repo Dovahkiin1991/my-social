1f6e63bfefbc328b0c33dc2984929237d11acb72
backflipper1991 <sashaulych@gmail.com>
2020-05-26 21:19:20 +0300

    Updates

---
+++ precache-manifest.f4bdda23bfbb420f26c01b7d215d7b62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619ac7adaa2fea1a1307fa2009cb858e",
+    "url": "/my-social/index.html"
+  },
+  {
+    "revision": "ea7d6066ec835c327b80",
+    "url": "/my-social/static/css/main.1ed75a01.chunk.css"
+  },
+  {
+    "revision": "8fdd25665bebaf288a28",
+    "url": "/my-social/static/js/2.e574789d.chunk.js"
+  },
+  {
+    "revision": "ea7d6066ec835c327b80",
+    "url": "/my-social/static/js/main.3fea5e0d.chunk.js"
+  },
+  {
+    "revision": "987ed9a2fb1b656c82e6",
+    "url": "/my-social/static/js/runtime~main.ea71a730.js"
+  }
+]);
